47fcb0fb1578de67f54c4d08c6218e4da642a261
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-12-18 17:08:07 +0200

    Configured project

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Strict mode forces components render twice, so it may break some functionality
+  reactStrictMode: false,
   /* config options here */
 };
 

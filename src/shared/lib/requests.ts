19a6979b83e1adb770d2ec08b50acc7b904cc978
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-12-19 22:36:37 +0200

    Refactored code

---
+++ src/shared/lib/requests.ts
@@ -0,0 +1,50 @@
+export const fetchRecords = async () => {
+  try {
+    const res = await fetch('/api/records', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const receivedRecords = await res.json()
+    return receivedRecords.records
+  } catch (err) {
+    console.error(err)
+    if (err instanceof Error) {
+      throw err
+    }
+  }
+}
+
+export const transcriptAudio = async (payload: FormData) => {
+  try {
+    const requestOptions = { method: 'POST', body: payload }
+
+    const res = await fetch('/api/speech-to-text', requestOptions)
+    const receivedTranscription = await res.json()
+
+    return receivedTranscription.transcription
+  } catch (err) {
+    console.error(err)
+    if (err instanceof Error) {
+      throw err
+    }
+  }
+}
+
+export const checkIfSubscribed = async (payload: FormData) => {
+  try {
+    const res = await fetch('/api/subscription/check', {
+      method: 'POST',
+      body: payload,
+    })
+
+    return await res.json()
+  } catch (err) {
+    console.error(err)
+    if (err instanceof Error) {
+      throw err
+    }
+  }
+}

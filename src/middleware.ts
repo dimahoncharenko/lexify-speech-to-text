58cf9edf1141ea84b7a8f9f8f3785525f0279143
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-12-20 17:10:50 +0200

    Unlocked /subscribe-user

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-in(.*)',
   '/sign-up(.*)',
   '/api/save-user',
+  '/api/subscribe-user',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
